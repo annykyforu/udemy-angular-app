37502688397f516723e77a38a45e842a73ea3363
Andrii Nykyforuk <18334176+annykyforu@users.noreply.github.com>
2023-04-23 22:07:16 +0200

    cleaned up code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,22 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {ServerComponent} from "./server/server.component";
 import {ServersComponent} from './servers/servers.component';
-import {WarningalertComponent} from './warningalert/warningalert.component';
-import {SuccessalertComponent} from './successalert/successalert.component';
+import {WarningAlertComponent} from './warning-alert/warning-alert.component';
+import {SuccessAlertComponent} from './success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningalertComponent,
-    SuccessalertComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ServerComponent,
+        ServersComponent,
+        WarningAlertComponent,
+        SuccessAlertComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
